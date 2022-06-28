d89eca6029e8026261d8425efe1488508f57cea2
RedStar08 <redstar08@qq.com>
2022-06-28 23:55:14 +0800

    init & lint file

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: false,
+  proseWrap: "never",
+  endOfLine: "auto",
+  trailingComma: "all",
+  quoteProps: "as-needed",
+}
